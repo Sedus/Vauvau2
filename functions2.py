1a84000e8acbd356dd0be9660a2e1cc30fba48aa
Sedus <76965026+Sedus@users.noreply.github.com>
2022-11-17 23:00:21 +0100

    Add files via upload

--- functions2.py
+++ functions2.py
@@ -1,14 +1,15 @@
 import os
 import sys
-from termcolor import cprint
+from termcolor import cprint,colored
 import time
 from art import tprint
 import random
+import rainbowtext
 
 import variables2
 
 def draw():
-    print ("==============================================================================")
+    cprint ("==============================================================================")
 
 def listToString(s):
     # initialize an empty string
@@ -39,54 +40,55 @@ def toplay():
 
 def start():
     tprint("vauvau    2", "tarty1")
-    tprint("PRE-ALPHA    0.0.1", "tarty1")
-    print("NYOMJ MEG BÁRMIT AZ INDULÁSHOZ!")
+    tprint("PRE-ALPHA    0.0.2", "tarty1")
+    cprint("NYOMJ MEG BÁRMIT AZ INDULÁSHOZ!")
     input("> ")
 
 def rules():
-    print("Számok beírásával tudsz navigálni." + "\n" + "A számokhoz tartozó parancsot mellette írva találod.")
+    cprint("Számok beírásával tudsz navigálni." + "\n" + "A számokhoz tartozó parancsot mellette írva találod.")
     input("> ")
 
 def menu_layout():
     clear()
 
-    effect("1 - ÚJ JÁTÉK\n", "green", 0.01)
-    effect("2 - JÁTÉK BETÖLTÉSE\n", "blue", 0.01)
-    effect("3 - SZABÁLYOK\n", "magenta", 0.01)
-    effect("4 - KILÉPÉS\n", "yellow", 0.01)
+    cprint("1 - ÚJ JÁTÉK", "green")
+    cprint("2 - JÁTÉK BETÖLTÉSE", "blue")
+    cprint("3 - SZABÁLYOK", "magenta")
+    cprint("4 - KILÉPÉS", "yellow")
 
     draw()
 
 def menu_layout2():
-    print("JELENLEGI POZÍCIÓ: " + variables2.biom[variables2.map[variables2.y][variables2.x]]["name"])
+    cprint("JELENLEGI POZÍCIÓ: " + variables2.biom[variables2.map[variables2.y][variables2.x]]["name"], "magenta")
     draw()
-    effect("NÉV: " + variables2.name + "\n", "blue", 0.01)
-    effect("ÉLET:" + "|♥♥♥♥♥♥♥♥♥♥|" + str(variables2.player_hp) + "/" + str(variables2.player_hpmax) + "\n", "green", 0.01)
-    effect("SEBZÉS:" + "|¤¤¤¤¤¤¤¤¤¤|" + str(variables2.player_atk) + "\n", "cyan", 0.01)
-    effect("GYÓGYITAL: " + str(variables2.pot) + " darab\n", "red", 0.01)
-    effect("ELIXÍR: " +str(variables2.elix) + " darab\n", "magenta", 0.01)
-    effect("ARANY: " +str(variables2.gold) + "$\n", "yellow", 0.01)
+    cprint("NÉV: " + variables2.name, "blue")
+    cprint("ÉLET:" + "|♥♥♥♥♥♥♥♥♥♥|" + str(variables2.player_hp) + "/" + str(variables2.player_hpmax), "green")
+    cprint("SEBZÉS:" + "|¤¤¤¤¤¤¤¤¤¤|" + str(variables2.player_atk), "cyan")
+    cprint("GYÓGYITAL: " + str(variables2.pot) + " darab", "red")
+    cprint("ELIXÍR: " + str(variables2.elix) + " darab", "magenta")
+    cprint("ARANY: " + str(variables2.gold) + "$", "yellow")
     draw()
-    effect("0 - MENTÉS ÉS KILÉPÉS\n", "green", 0.01)
+    cprint("0 - MENTÉS ÉS KILÉPÉS", "green")
+
     if variables2.y > 0:
-        effect("1 - ▲ FEL\n", "green", 0.01)
+        cprint("1 - ▲ FEL", "green")
     if variables2.x < variables2.x_len:
-        effect("2 - ► JOBBRA\n", "green", 0.01)
+        cprint("2 - ► JOBBRA", "green")
     if variables2.y < variables2.y_len:
-        effect("3 - ▼ LE\n", "green", 0.01)
+        cprint("3 - ▼ LE", "green")
     if variables2.x > 0:
-        effect("4 - ◄ BALRA\n", "green", 0.01)
+        cprint("4 - ◄ BALRA", "green")
     if variables2.pot > 0:
-        effect("5 - GYÓGYITAL HASZNÁLATA (30HP)\n", "green", 0.01)
+        cprint("5 - GYÓGYITAL HASZNÁLATA (30HP)", "green")
     if variables2.elix > 0:
-        effect("6 - ELIXÍR HASZNÁLATA (MAXHP)\n", "green", 0.01)
+        cprint("6 - ELIXÍR HASZNÁLATA (MAXHP)", "green")
     if variables2.map[variables2.y][variables2.x] == "bolt":
-        effect("7 - BELÉPÉS\n", "green", 0.01)
+        cprint("7 - BELÉPÉS", "green")
     draw()
 
 def heal(amount):
     variables2.player_hp += amount
-    print (variables2.name + " élete fel lett töltve " + str(variables2.player_hp) + "-re!")
+    cprint (variables2.name + " élete fel lett töltve " + str(variables2.player_hp) + "-re!", "green")
     input("> ")
 
 def usepotion():
@@ -97,7 +99,7 @@ def usepotion():
         else:
             heal(30)
     else:
-        print("Nincs gyógyitalod!")
+        cprint("Nincs gyógyitalod!", "green")
         input("> ")
 
 def useelixir():
@@ -105,7 +107,7 @@ def useelixir():
         variables2.elix -= 1
         heal(variables2.player_hpmax - variables2.player_hp)
     else:
-        print("Nincs elixíred!")
+        cprint("Nincs elixíred!", "green")
         input("> ")
 
 def createmap():
@@ -166,7 +168,7 @@ def toshop():
 def shoplayout():
     clear()
     draw()
-    print("Üdvözöllek a boltban!")
+    cprint("Üdvözöllek a boltban!")
     draw()
     cprint("SEBZÉS:" + "|¤¤¤¤¤¤¤¤¤¤|" + str(variables2.player_atk), "cyan")
     cprint("GYÓGYITAL: " + str(variables2.pot) + " darab", "red")
@@ -174,37 +176,37 @@ def shoplayout():
     cprint("ARANY: " +str(variables2.gold) + "$", "yellow")
 
     draw()
-    effect("1 - FEGYVER FEJLESZTÉSE (+2 SEBZÉS) - 10 ARANY\n", "cyan", 0.01)
-    effect("2 - GYÓGYITAL VÁSÁRLÁSA (+30HP) - 5 ARANY\n", "red", 0.01)
-    effect("3 - ELIXÍR VÁSÁRLÁSA (MAXHP) - 20 ARANY\n", "magenta", 0.01)
-    effect("4 - KILÉPÉS\n", "yellow", 0.01)
+    cprint("1 - FEGYVER FEJLESZTÉSE (+2 SEBZÉS) - 10 ARANY", "cyan")
+    cprint("2 - GYÓGYITAL VÁSÁRLÁSA (+30HP) - 5 ARANY", "red")
+    cprint("3 - ELIXÍR VÁSÁRLÁSA (MAXHP) - 20 ARANY", "magenta")
+    cprint("4 - KILÉPÉS", "yellow")
     draw()
 
 def weaponupgrade():
     if variables2.gold >= 10:
         variables2.player_atk += 2
         variables2.gold -= 10
-        print ("Sikeresen fejlesztetted a fegyvered!")
+        cprint ("Sikeresen fejlesztetted a fegyvered!", "green")
     else:
-        print ("Nincs elég aranyad!")
+        cprint ("Nincs elég aranyad!", "green")
     input("> ")
 
 def potbuy():
     if variables2.gold >= 5:
         variables2.pot += 1
         variables2.gold -= 5
-        print ("Vettél egy gyógyitalt!")
+        cprint ("Vettél egy gyógyitalt!", "green")
     else:
-        print ("Nincs elég aranyad!")
+        cprint ("Nincs elég aranyad!", "green")
     input("> ")
 
 def elixbuy():
     if variables2.gold >= 20:
         variables2.elix += 1
         variables2.gold -= 20
-        print ("Vettél egy elixírt!")
+        cprint ("Vettél egy elixírt!", "green")
     else:
-        print ("Nincs elég aranyad!")
+        cprint ("Nincs elég aranyad!", "green")
     input("> ")
 
 def spawnenemychance():
@@ -222,37 +224,37 @@ def spawnenemychance():
 def battlelayout():
     clear()
     draw()
-    print("Győzd le " + variables2.mobs [variables2.enemy] ["name"] + "-ot!")
+    cprint("Győzd le " + variables2.mobs [variables2.enemy] ["name"] + "-ot!", "green")
     draw()
-    print(variables2.mobs [variables2.enemy] ["name"] + " élete: " + str(variables2.enemy_hp))
-    print(variables2.name + " élete: " + str(variables2.player_hp) + "/" + str(variables2.player_hpmax))
+    cprint(variables2.mobs [variables2.enemy] ["name"] + " élete: " + str(variables2.enemy_hp), "green")
+    cprint(variables2.name + " élete: " + str(variables2.player_hp) + "/" + str(variables2.player_hpmax), "green")
     draw()
-    print("GYÓGYITAL: " + str(variables2.pot) + " darab")
-    print("ELIXÍR: " +str(variables2.elix) + " darab")
+    cprint("GYÓGYITAL: " + str(variables2.pot) + " darab", "green")
+    cprint("ELIXÍR: " +str(variables2.elix) + " darab", "green")
     draw()
-    print("1 - TÁMADÁS")
-    print("2 - GYÓGYITAL HASZNÁLATA (30HP)")
-    print("3 - ELIXÍR HASZNÁLATA (MAXHP)")
+    cprint("1 - TÁMADÁS", "green")
+    cprint("2 - GYÓGYITAL HASZNÁLATA (30HP)", "green")
+    cprint("3 - ELIXÍR HASZNÁLATA (MAXHP)", "green")
     draw()
 
 def attack():
     variables2.enemy_hp -= variables2.player_atk
     variables2.player_hp -= variables2.enemy_atk
-    print (variables2.name + " " + str(variables2.player_atk) + " sebzést okozott " + variables2.mobs [variables2.enemy] ["name"] + "-nak.")
-    print (variables2.mobs [variables2.enemy] ["name"] + " " + str(variables2.enemy_atk) + " sebzést okozott " + variables2.name + "-nak.")
+    cprint (variables2.name + " " + str(variables2.player_atk) + " sebzést okozott " + variables2.mobs [variables2.enemy] ["name"] + "-nak.", "green")
+    cprint (variables2.mobs [variables2.enemy] ["name"] + " " + str(variables2.enemy_atk) + " sebzést okozott " + variables2.name + "-nak.", "green")
     input("> ")
     if variables2.enemy_hp <= 0:
         loot()
     if variables2.player_hp <= 0:
         clear()
-        print (variables2.mobs [variables2.enemy] ["name"] + " legyőzte " + variables2.name + "...")
-        print("VÉGE")
+        cprint (variables2.mobs [variables2.enemy] ["name"] + " legyőzte " + variables2.name + "...", "red")
+        cprint("VÉGE", "red")
         input("> ")
         quit()
 
 def loot():
     clear()
-    print (variables2.name + " legyőzte " + (variables2.mobs [variables2.enemy] ["name"] + "-ot!"))
-    print ("Találtál " + str(variables2.mobs [variables2.enemy] ["gold"]) + " aranyat!")
+    cprint (variables2.name + " legyőzte " + (variables2.mobs [variables2.enemy] ["name"] + "-ot!"), "green")
+    cprint ("Találtál " + str(variables2.mobs [variables2.enemy] ["gold"]) + " aranyat!", "green")
     input("> ")
     toplay()
